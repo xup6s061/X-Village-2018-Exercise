540f8107b63b8d50e490cfbead19ee7c07a8966e
xup6s061 <xup6s061@gmail.com>
2018-07-17 21:11:26 +0800

    final

--- Lesson05-Exception_Basics/ex4.py
+++ Lesson05-Exception_Basics/ex4.py
@@ -6,18 +6,11 @@ class RelationException(Exception):
 
 relation = {'Jason':'Mary', 'Mary':'Jason', 'Jennifer':'Ken', 'Ken':'Jennifer', 'Tina':'Kim', 'Kim':'Tina'}
 def check(pa, pb):
-    if relation[pa] != pb:
-        for i in relation:
-            for j in relation:
-                if relation[i]==pa and relation[j]==pb:
-                    raise RelationException('Are you sure that '+pa +' and ' +pb +' are in love with each other?')
-                    break
-            a=0
-            if relation[i]==pa or relation[i]==pb:
-                a+=1
-            if a>0:
-                raise RelationException('No relation found'+'\n'+'Are you sure that '+pa +' and ' +pb +' are in love with each other?')
-
+    try:
+        if relation[pa] != pb and relation[pb] !=pa:
+            raise RelationException('Are you sure that '+pa +' and ' +pb +' are in love with each other?')
+    except KeyError:
+        raise RelationException('No relation found'+'\n'+'Are you sure that '+pa +' and ' +pb +' are in love with each other?')
 try:
     p1 = input("Please enter the first person: ")
     p2 = input("Please enter the second person: ")
